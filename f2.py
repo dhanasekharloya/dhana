c490591b2c20f53f209e348383f6a3c53ae3c966
dhana sekhar <dhanasekharloya@gmail.com>
2022-11-22 00:36:25 +0530

    hy

--- f2.py
+++ f2.py
@@ -1,2 +1,11 @@
-x="hello world"
-print(x)
+num1=int(input("enter num1 value:"))
+num2=int(input("enter num2 value:"))
+for i in range(num1,num2+1):
+    no_of_digits=len(str(i))
+    temp=i
+    s=0
+    dig=temp%10
+    s+=dig**no_of_digits
+    temp//=10
+if i==s:
+    print()
